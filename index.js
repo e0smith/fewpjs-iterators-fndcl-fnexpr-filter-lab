27b1d0da4abc2a32c6b498f606139c92ff9e7f91
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-19 18:45:54 -0400

    commit tree b5b673ef6f5f179441b13660b21c639ab80ef41f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    let match = drivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+    return match;
+};
+
+function fuzzyMatch(drivers, string) {
+    let match = drivers.filter(driver => {
+        return driver.startsWith(string)
+    });
+    return match;
+};
+
+function matchName(drivers, string) {
+    let match = drivers.filter(driver => {
+        return driver.name === string
+    });
+    return match;
+};
